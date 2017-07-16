66ff74f96d9c7dc7121403a4bc952461384d284f
pgagliar <pierre.gagliardi@mfglabs.com>
2017-07-16 18:14:51 +0200

    Styling Example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require("react");
+var {Link}=require("react-router");
 
 var Examples = (props) =>{
   return(
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the about pages</p>
+      <h1 className = "text-center">Examples</h1>
+      <p>Here a are a few examples locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Nice'>Nice</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio</Link>
+        </li>
+      </ol>
     </div>
   );
 };
